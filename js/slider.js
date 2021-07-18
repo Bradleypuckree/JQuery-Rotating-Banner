eb5479165c30af6f3c6f9eb3b31c3372902fa62d
Bradley Puckree <bradleypuckree@gmail.com>
2021-07-18 14:55:47 +0200

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,33 @@
+var currentIndex = 0;// store current pane index displayed
+var ePanes = $('#slider .panel'), // store panes collection
+    time   = 6000,
+    bar = $('.progress_bar');
+
+function showPane(index){// generic showPane
+    // hide current pane
+    ePanes.eq(currentIndex).stop(true, true).fadeOut();
+    // set current index : check in panes collection length
+    currentIndex = index;
+    if(currentIndex < 0) currentIndex = ePanes.length-1;
+    else if(currentIndex >= ePanes.length) currentIndex = 0;
+    // display pane
+    ePanes.eq(currentIndex).stop(true, true).fadeIn();
+    // menu selection
+    $('.nav li').removeClass('current').eq(currentIndex).addClass('current');
+}
+// bind ul links
+$('.nav li').click(function(ev){    showPane($(this).index());});
+// bind previous & next links
+$('.previous').click(function(){    showPane(currentIndex-1);});
+$('.next').click(function(){    showPane(currentIndex+1);});
+// apply start pane
+showPane(0);
+
+function run(){
+    bar.width(0);
+    showPane(currentIndex+1);
+    bar.animate({'width': "+=100%"}, time, run);
+}
+
+run();
+
